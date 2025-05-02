c941199033c1641d75f8f014149fb7c0595fc759
Andrew Anderson <andy@clubanderson.com>
2025-05-01 23:58:15 -0400

    steps to automate some config for use in testing

--- hack/setup/steps/02_clone_fmperf.sh
+++ hack/setup/steps/02_clone_fmperf.sh
@@ -9,6 +9,7 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate "$CONDA_ENV_NAME"
 pip install -r requirements.txt
 pip install -e .
+
 docker build -t fmperf .
 mkdir -p requests && chmod o+w requests
 cp .env.example .env
