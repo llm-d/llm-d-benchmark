9ecf6ca5a9b0d9a7f297e7de8338e2b797c67e09
Diego-Castan <diego.castan@ibm.com>
2025-05-02 17:09:21 -0400

    miniconda

--- hack/setup/steps/01_install_conda.sh
+++ hack/setup/steps/01_install_conda.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
-source "$(dirname "$0")/env.sh"
+# source "$(dirname "$0")/env.sh"
 
 echo "Installing Anaconda..."
 if ! command -v conda &>/dev/null; then
-  brew install anaconda
-  ANACONDA_PATH='export PATH="/opt/homebrew/anaconda3/bin:$PATH"'
+  # Check if the system is macOS
+  if [ "$(uname)" == "Darwin" ]; then
+    echo "Installing Miniforge for macOS..."
+    brew install --cask miniforge
+  else
+    # For Linux, you can use the official Miniforge installer script
+    echo "Installing Miniforge for Linux..."
+
+    # Download and run the installer
+    MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname -m)-$(uname -s).sh"
+    wget -qO - $MINIFORGE_URL | bash -b -P /opt/miniconda
+  fi
+
+  ANACONDA_PATH='export PATH="/opt/homebrew/bin/conda:$PATH"'
   if ! grep -Fxq "$ANACONDA_PATH" ~/.zshrc; then
     echo "$ANACONDA_PATH" >> ~/.zshrc
     echo "✅ Anaconda path added to ~/.zshrc"
