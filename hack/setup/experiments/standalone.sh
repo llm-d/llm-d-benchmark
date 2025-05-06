1058a4a601f95941d7259e8be213b83d94bf94bc
Andrew Anderson <andy@clubanderson.com>
2025-05-06 17:30:26 -0400

    adding the beginning of experiments

---
+++ hack/setup/experiments/standalone.sh
@@ -0,0 +1,12 @@
+./deploy.sh --step 00
+./deploy.sh --step 01
+./deploy.sh --step 02
+./deploy.sh --step 03
+./deploy.sh --step 04
+./deploy.sh --step 05
+./deploy.sh --step 06
+# ./deploy.sh --step 07
+./deploy.sh --step 08
+# ./deploy.sh --step 09
+# ./deploy.sh --step 10
+./deploy.sh --step 11
