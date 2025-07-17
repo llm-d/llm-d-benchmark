f94f75e128c0aa8128a3ca8702bff63c04716b8c
maugustosilva <maugusto.silva@gmail.com>
2025-07-17 18:49:20 -0400

    [Setup] feat: initial steps on the integration of `llm-d-infra`
    With the impending retirement of `llm-d-deployer`, our code needs to be
    modified to operate with the new `llm-d-infra`. The following
    modifications are implemented:
    
    1) Clone `llm-d-infra` (`llm-d-deployer` is still there, but only
       temporarily)
    2) Use `llmd-infra-installer` to deploy the base gateway infrastructure
       (the `helm` chart implicit invocation is expected to be idempotent).
    At this point, a `helmfile` is also created
    3) Add a **new** step (7), where the `gateway inference endpoint` (gaie)
       is deployed (with the use of `helmfile`)
    4) An initial version of the generation for the `model service`-specific
       `values.yaml` was also provided. This was added to
    `deploy_via_modelservice.sh` (from PR #122), but not fully integrated
    yet.
    5) Several per-`pod` limits were added, allowing an user to specify
       different values for `prefill` and `decode` `pods`
    6) Finally, the function `model_attribute` was enhanced
    
    **IMPORTANT**: the executable `helmfile` is now required
    
    Signed-off-by: maugustosilva <maugusto.silva@gmail.com>

--- setup/steps/00_ensure_llm-d-infra.sh
+++ setup/steps/00_ensure_llm-d-infra.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 source ${LLMDBENCH_CONTROL_DIR}/env.sh
 
+announce "💾 Cloning and setting up llm-d-infra..."
+pushd $LLMDBENCH_INFRA_DIR &>/dev/null
+if [[ ! -d llm-d-infra ]]; then
+  llmdbench_execute_cmd "cd ${LLMDBENCH_INFRA_DIR}; git clone \"${LLMDBENCH_INFRA_GIT_REPO}\" -b \"${LLMDBENCH_INFRA_GIT_BRANCH}\"" ${LLMDBENCH_CONTROL_DRY_RUN} ${LLMDBENCH_CONTROL_VERBOSE}
+else
+  pushd llm-d-infra &>/dev/null
+  llmdbench_execute_cmd "git checkout ${LLMDBENCH_INFRA_GIT_BRANCH}; git pull" ${LLMDBENCH_CONTROL_DRY_RUN} ${LLMDBENCH_CONTROL_VERBOSE}
+  popd &>/dev/null
+fi
+popd &>/dev/null
+announce "✅ llm-d-infra is present at \"${LLMDBENCH_DEPLOYER_DIR}\""
+
 announce "💾 Cloning and setting up llm-d-deployer..."
 pushd $LLMDBENCH_DEPLOYER_DIR &>/dev/null
 if [[ ! -d llm-d-deployer ]]; then
