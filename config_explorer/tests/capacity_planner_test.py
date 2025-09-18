57fb757864262f731d70e47ae323354847ce0418
Jing Chen <jing.chen2@ibm.com>
2025-09-18 19:11:09 -0400

    Clean up
    Signed-off-by: Jing Chen <jing.chen2@ibm.com>

--- config_explorer/tests/capacity_planner_test.py
+++ config_explorer/tests/capacity_planner_test.py
@@ -2,9 +2,6 @@
 Tests Capacity Planner functions
 """
 
-import pytest
-from huggingface_hub import ModelInfo
-from transformers import AutoConfig
 from src.config_explorer.capacity_planner import *
 
 # ---- Constants ----
@@ -117,73 +114,6 @@ def test_inference_dtype():
     deepseek = "deepseek-ai/DeepSeek-R1"
     model_config = get_model_config_from_hf(deepseek)
     assert inference_dtype(model_config) == "torch.bfloat16"
-
-
-# def test_model_memory_req():
-#     """
-#     Tests that model memory is correct calculated for the precision type
-#     """
-#     for precision_type in precision_types:
-
-#         # Start with 5 million parameters
-#         parameter = 5000000
-
-#         # Go up to 500 billion parameters
-#         # 500 billion = 500,000,000,000
-#         for _ in range(5):
-#             model_info = ModelInfo(
-#                 id=small_model_id,
-#                 safetensors={
-#                     "parameters": {
-#                         precision_type: parameter,
-#                     },
-#                     "total": parameter
-#                 }
-#             )
-
-#             actual_model_memory = model_memory_req(model_info)
-#             expected_model_memory = 0
-#             match precision_type:
-#                 case "fp32":
-#                     expected_model_memory = (parameter * 4 * 1.2) / (1024 ** 3)
-#                 case "fp16":
-#                     expected_model_memory = (parameter * 2 * 1.2) / (1024 ** 3)
-#                 case "fp8":
-#                     expected_model_memory = (parameter * 1 * 1.2) / (1024 ** 3)
-#                 case "int4":
-#                     expected_model_memory = (parameter * 0.5 * 1.2) / (1024 ** 3)
-
-#             assert actual_model_memory == expected_model_memory
-#             parameter *= 10
-
-# def test_gpu_min_req():
-#     """
-#     Tests that the minimum GPU required is correctly calculated
-#     """
-#     # Start with 5 million parameters
-#     parameter = 5000000
-
-#     # Go up to 500 billion parameters
-#     # 500 billion = 500,000,000,000
-#     for _ in range(5):
-#         model_info = ModelInfo(
-#             id=small_model_id,
-#             safetensors={
-#                 "parameters": {
-#                     precision_types[0]: parameter,
-#                 },
-#                 "total": parameter
-#             }
-#         )
-
-#         # Start with gpu_memory=10, increment by 10 each time, up to 200
-#         for gpu_memory in range(10, 200, 10):
-#             model_memory = (parameter * 4 * 1.2) / (1024 ** 3)
-#             actual_min_gpu_req = min_gpu_req(model_info, gpu_memory)
-#             expected_min_gpu_req = math.ceil(model_memory / gpu_memory)
-#             assert actual_min_gpu_req == expected_min_gpu_req
-
-#         parameter *= 10
 
 def test_kv_cache_req():
     """
