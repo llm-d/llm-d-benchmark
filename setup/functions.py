6e4b1c4c637ed512305d0b9cd52810546027f0b3
mengmeiye <mye@ibm.com>
2025-11-25 13:56:13 -0500

    move load_kube_config out of wait_for_pods_created_running_ready()

--- setup/functions.py
+++ setup/functions.py
@@ -1631,7 +1631,7 @@ def add_scc_to_service_account(
             announce(f'Successfully updated SCC "{scc_name}"')
 
 
-def wait_for_pods_created_running_ready(ev: dict, component_nr: int, component: str) -> int:
+def wait_for_pods_created_running_ready(api_client, ev: dict, component_nr: int, component: str) -> int:
     """
     Wait for pods to be created, in Running state and then in Ready state.
     """
@@ -1642,14 +1642,12 @@ def wait_for_pods_created_running_ready(ev: dict, component_nr: int, component: 
         announce(
             f'⏳ Waiting for all ({component}) pods serving model to be in "Running" state (timeout={int(ev["control_wait_timeout"])}s)...'
         )
-        k8s_config.load_kube_config()
-        api_client = k8s_client.CoreV1Api()
+        w = k8s_watch.Watch()
         max_retries = 5
         delay = 2
         pod_create_list = []
         for attempt in range(max_retries):
             try:
-                w = k8s_watch.Watch()
                 pod_running_list = []
                 pod_ready_list = []
                 for event in w.stream(api_client.list_namespaced_pod, namespace=ev["vllm_common_namespace"], label_selector=f"llm-d.ai/model={ev['deploy_current_model_id_label']},llm-d.ai/role={component}", timeout_seconds=int(ev["control_wait_timeout"])):
