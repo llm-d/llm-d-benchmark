ccf357216766787581d493a78864580b50fc4837
maugustosilva <maugusto.silva@gmail.com>
2025-12-02 17:01:12 -0500

    Update setup/steps/10_smoketest.py
    Co-authored-by: Nick Masluk <144150872+namasl@users.noreply.github.com>
    Signed-off-by: maugustosilva <maugusto.silva@gmail.com>

--- setup/steps/10_smoketest.py
+++ setup/steps/10_smoketest.py
@@ -101,7 +101,7 @@ def check_deployment(api: pykube.HTTPClient, client: any, ev: dict):
             return 1
         else :
             if service_hostname == "N/A":
-                try :
+                try:
                     ipaddress.ip_address(service_ip)
                 except ValueError:
                     announce(f"ERROR: Invalid IP (\"{service_ip}\") for service/gateway \"{service_name}\"!")
