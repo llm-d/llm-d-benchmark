fff8cfec8ebeba659fb717597b1b4467f93398d6
Nick Masluk <nick@randombytes.net>
2026-01-07 18:09:54 -0500

    Update CLI usage for benchmark report conversion
    Signed-off-by: Nick Masluk <nick@randombytes.net>

--- benchmark_report/native_to_br.py
+++ benchmark_report/native_to_br.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python3
+"""
+Convert application native output formats into a Benchmark Report.
+"""
 
-# This script imports data from a benchmark run in llm-d-benchmark using any
-# supported harness, and converts the results into a data file with a standard
-# benchmark report format. This format can then be used for post processing
-# that is not specialized to a particular harness.
-
-import argparse
 import base64
 import datetime
 import os
@@ -1125,99 +1121,3 @@ def import_nop(results_file: str) -> BenchmarkReport:
     update_dict(br_dict, results_dict)
 
     return load_benchmark_report(br_dict)
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(
-        description='Convert benchmark run data to standard benchmark report format.')
-    parser.add_argument(
-        'results_file',
-        type=str,
-        help='Results file to convert.')
-    parser.add_argument(
-        'output_file',
-        type=str,
-        default=None,
-        nargs='?',
-        help='Output file for benchark report.')
-    parser.add_argument(
-        '-f', '--force',
-        action=argparse.BooleanOptionalAction,
-        help='Write to output file even if it already exists.')
-    parser.add_argument(
-        '-w', '--workload-generator',
-        type=str,
-        default=WorkloadGenerator.VLLM_BENCHMARK,
-        help=f'Workload generator used, one of: {str([member.value for member in WorkloadGenerator])[1:-1]}')
-    parser.add_argument(
-        '-i',
-        '--index',
-        type=int,
-        default=None,
-        help='Benchmark index to import, for results files containing multiple runs. Default behavior creates benchmark reports for all runs.')
-
-    args = parser.parse_args()
-    if args.output_file and os.path.exists(
-            args.output_file) and not args.force:
-        sys.stderr.write('Output file already exists: %s\n' % args.output_file)
-        sys.exit(1)
-
-    match args.workload_generator:
-        case WorkloadGenerator.GUIDELLM:
-            if args.index:
-                # Generate benchmark report for a specific index
-                if args.output_file:
-                    import_guidellm(
-                        args.results_file,
-                        args.index).export_yaml(
-                        args.output_file)
-                else:
-                    import_guidellm(args.results_file, args.index).print_yaml()
-            else:
-                br_list = import_guidellm_all(args.results_file)
-                # Generate reports for all runs
-                for ii, br in enumerate(br_list):
-                    if args.output_file:
-                        # Create a benchmark report file
-                        fname, ext = os.path.splitext(args.output_file)
-                        output_file = f'{fname}_{ii}{ext}'
-                        if os.path.exists(output_file) and not args.force:
-                            sys.stderr.write(
-                                'Output file already exists: %s\n' %
-                                output_file)
-                            sys.exit(1)
-                        br.export_yaml(output_file)
-                    else:
-                        # Don't create a file, just print to stdout
-                        print(f'# Benchmark {ii + 1} of {len(br_list)}')
-                        br.print_yaml()
-        case WorkloadGenerator.INFERENCE_PERF:
-            if args.output_file:
-                import_inference_perf(
-                    args.results_file).export_yaml(args.output_file)
-            else:
-                import_inference_perf(args.results_file).print_yaml()
-        case WorkloadGenerator.VLLM_BENCHMARK:
-            if args.output_file:
-                import_vllm_benchmark(
-                    args.results_file).export_yaml(args.output_file)
-            else:
-                import_vllm_benchmark(args.results_file).print_yaml()
-        case WorkloadGenerator.INFERENCE_MAX:
-            if args.output_file:
-                import_inference_max(
-                    args.results_file).export_yaml(args.output_file)
-            else:
-                import_inference_max(args.results_file).print_yaml()
-        case WorkloadGenerator.NOP:
-            if args.output_file:
-                import_nop(args.results_file).export_yaml(args.output_file)
-            else:
-                import_nop(args.results_file).print_yaml()
-        case _:
-            sys.stderr.write('Unsupported workload generator: %s\n' %
-                             args.workload_generator)
-            sys.stderr.write('Must be one of: %s\n' %
-                             str([wg.value for wg in WorkloadGenerator])[1:-1])
-            sys.exit(1)
