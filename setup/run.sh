61ca2d3488563a324971495d7b5f915bc8deec00
vknaik <vknaik@us.ibm.com>
2026-02-05 17:33:24 -0500

    fix: Quote path variables in shell scripts to handle spaces
    - Fixed unquoted path variables causing word splitting in paths with spaces
    - Updated standup.sh: quoted pushd, realpath, source, find, and sed expressions
    - Updated env.sh: quoted source, find, mkdir, rm, cp, touch, ls commands
    - Updated functions.sh: quoted extensive path operations (mkdir, rm, cp, touch, cat, ls, find, rsync)
    - Updated run.sh: quoted pushd, realpath, source, find, mkdir, rm, ls commands
    - Updated teardown.sh: quoted pushd, realpath, source commands
    - Updated e2e.sh: quoted pushd, realpath, source, find, ls commands
    
    Resolves issue where standup.sh and other scripts would fail when run from
    directories containing spaces in their path.
    
    fix: Quote path variable in util/setup_precommit.sh

--- setup/run.sh
+++ setup/run.sh
@@ -214,7 +214,7 @@ done
 
 export LLMDBENCH_CONTROL_CLI_OPTS_PROCESSED=1
 
-source ${LLMDBENCH_CONTROL_DIR}/env.sh
+source "${LLMDBENCH_CONTROL_DIR}/env.sh"
 
 export LLMDBENCH_BASE64_CONTEXT_CONTENTS=$LLMDBENCH_CONTROL_WORK_DIR/environment/context.ctx
 
@@ -415,7 +415,7 @@ for method in ${LLMDBENCH_DEPLOY_METHODS//,/ }; do
         fi
       fi
 
-      rm -rf ${LLMDBENCH_CONTROL_WORK_DIR}/workload/profiles/*
+rm -rf "${LLMDBENCH_CONTROL_WORK_DIR}/workload/profiles/"*
 
       if [[ $LLMDBENCH_HARNESS_DEBUG -eq 1 ]]; then
 
@@ -444,18 +444,18 @@ for method in ${LLMDBENCH_DEPLOY_METHODS//,/ }; do
 
       for workload_type in ${LLMDBENCH_HARNESS_PROFILE_HARNESS_LIST}; do
         llmdbench_execute_cmd "${LLMDBENCH_CONTROL_KCMD} --namespace ${LLMDBENCH_HARNESS_NAMESPACE} delete configmap $workload_type-profiles --ignore-not-found" ${LLMDBENCH_CONTROL_DRY_RUN} ${LLMDBENCH_CONTROL_VERBOSE}
-        llmdbench_execute_cmd "${LLMDBENCH_CONTROL_KCMD} --namespace ${LLMDBENCH_HARNESS_NAMESPACE} create configmap $workload_type-profiles --from-file=${LLMDBENCH_CONTROL_WORK_DIR}/workload/profiles/${workload_type}" ${LLMDBENCH_CONTROL_DRY_RUN} ${LLMDBENCH_CONTROL_VERBOSE}
+  llmdbench_execute_cmd "${LLMDBENCH_CONTROL_KCMD} --namespace ${LLMDBENCH_HARNESS_NAMESPACE} create configmap $workload_type-profiles --from-file=\"${LLMDBENCH_CONTROL_WORK_DIR}/workload/profiles/${workload_type}\"" ${LLMDBENCH_CONTROL_DRY_RUN} ${LLMDBENCH_CONTROL_VERBOSE}
       done
 
       export LLMDBENCH_RUN_EXPERIMENT_ID_PREFIX=""
 
-      for treatment in $(ls ${LLMDBENCH_CONTROL_WORK_DIR}/workload/profiles/${workload_type}/*.yaml); do
+for treatment in $(ls "${LLMDBENCH_CONTROL_WORK_DIR}/workload/profiles/${workload_type}/"*.yaml); do
 
         export LLMDBENCH_RUN_EXPERIMENT_HARNESS_WORKLOAD_NAME=$(echo $treatment | rev | cut -d '/' -f 1 | rev)
         export LLMDBENCH_HARNESS_EXPERIMENT_PROFILE=$(echo $treatment | rev | cut -d '/' -f 1 | rev)
 
         tf=$(cat ${treatment} | grep "#treatment" | tail -1 | $LLMDBENCH_CONTROL_SCMD 's/^#//' || true)
-        if [[ -f ${LLMDBENCH_CONTROL_WORK_DIR}/workload/profiles/${workload_type}/treatment_list/$tf ]]; then
+  if [[ -f "${LLMDBENCH_CONTROL_WORK_DIR}/workload/profiles/${workload_type}/treatment_list/$tf" ]]; then
           tid=$(sed -e 's/[^[:alnum:]][^[:alnum:]]*/_/g' <<<"${tf%.txt}")   # remove non alphanumeric and .txt
           tid=${tid#treatment_}
           if [ -z "${LLMDBENCH_RUN_EXPERIMENT_ID}" ]; then
@@ -482,7 +482,7 @@ for method in ${LLMDBENCH_DEPLOY_METHODS//,/ }; do
 
           local_results_dir=${LLMDBENCH_CONTROL_WORK_DIR}/results/${LLMDBENCH_RUN_EXPERIMENT_RESULTS_DIR_SUFFIX}
           local_analysis_dir=${LLMDBENCH_CONTROL_WORK_DIR}/analysis/${LLMDBENCH_RUN_EXPERIMENT_RESULTS_DIR_SUFFIX}
-          llmdbench_execute_cmd "mkdir -p ${local_results_dir}_${i} && mkdir -p ${local_analysis_dir}_${i}" \
+  llmdbench_execute_cmd "mkdir -p \"${local_results_dir}_${i}\" && mkdir -p \"${local_analysis_dir}_${i}\"" \
                 ${LLMDBENCH_CONTROL_DRY_RUN} \
                 ${LLMDBENCH_CONTROL_VERBOSE}
 
